e6d043a3a9a7bcef2caabb194335c83311577889
Daniel Kang <danielkang@Daniels-iMac.fios-router.home>
2022-02-15 21:15:23 -0500

    Just saving my work

--- python/createbaseline.py
+++ python/createbaseline.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 MOD_LIST="Puppetfile"
-env_name="AcmeEnv"
+software_name="AcmeDS"
+GIT_REPO="git@github.com:kandang1/test.git"
 
-environment = input("Enter the environment name ")
+environment = input("Enter the environment name and version eg: AcmeDS_1_0_0 ")
 
-print(f"Hi you put in {environment} ")
+puppet_menu_option = {
+    1: 'PuppetDev',
+    2: 'PuppetProduction',
+}
 
-if e in environment:
-    print("yeah theres e")
-else:
-    print("nope nothing")
+def askPuppetServer(f):
+    print(f"yo i'm in this function with {f}")
+    for key in puppet_menu_option.keys():
+        print (key, '--', puppet_menu_option[key])
+
+while(True):
+    if software_name in environment:
+        askPuppetServer(environment)
+        break
+    else:
+        print("This isn't an AcmeDS software baseline. Try again.")
