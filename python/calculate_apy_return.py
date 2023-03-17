664ce2e796da8cfffd37d55c8a7aadb11996a552
Daniel Kang <danielkang@Daniels-iMac.fios-router.home>
2023-03-16 20:34:57 -0400

    calculate_apy_return.py: Calculate your APY for the entire year as well

--- python/calculate_apy_return.py
+++ python/calculate_apy_return.py
@@ -1,8 +1,12 @@
 #!/usr/local/bin/python3
 # Author: Dan Kang
 
-""" simple script to calculate the return based on APY
+""" script to calculate the return based on APY
 """
+
+import decimal
+
+MONTHS = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
 
 def calculate_monthly_interest_rate_to_decimal(apy):
     """ Returns the monthly interest rate based on the apy input"""
@@ -23,10 +27,32 @@ def ask_inputs(printtext):
 
 APY = ask_inputs('What is the APY ? ')
 PRINCIPAL = ask_inputs('What is the principal amount ? ')
+INITIAL_PRINCIPAL = PRINCIPAL
+
+def compound_monthly(apy,principal,month):
+#add the principal to the monthly interest and return that new number
+  interestEarned = (apy*principal)+principal
+  print(f'For the month of {month}, your account balance is', round(interestEarned,2))
+  return MONTHLY_COMPOUNDED_BALANCES.append(interestEarned)
 
 # Calculate and print monthly interest rate
 print(f"\nBased on an APY of {APY} and a principal amount of {PRINCIPAL}, the monthly interest rate in decimal is :", calculate_monthly_interest_rate_to_decimal(APY)) # pylint: disable=line-too-long
 # Convert and print the monthly rate back into percentage
 monthlyInterest = calculate_monthly_interest_rate_to_decimal(APY) * PRINCIPAL
+print("\nThe monthly interest gained without rounding is: $",monthlyInterest, " per month",sep='')
+
 print("\nThe monthly interest gained is: $",round(monthlyInterest,2), " per month",sep='')
 print("\nThe monthly interest rate is: ",round(calculate_monthly_interest_rate_to_decimal(APY)*100,2), "% per month",sep='') # pylint: disable=line-too-long
+
+# Now compound it 12 times to get the total for 12 months / 1 year
+MONTHLY_COMPOUNDED_BALANCES = []
+MONTHLY_COMPOUNDED_BALANCES.append(PRINCIPAL)
+#print(MONTHLY_COMPOUNDED_BALANCES)
+
+for i in range(12):
+  compound_monthly(calculate_monthly_interest_rate_to_decimal(APY),MONTHLY_COMPOUNDED_BALANCES[i],MONTHS[i])
+
+FINAL_SUM = MONTHLY_COMPOUNDED_BALANCES[-1]
+rounded_sum = round(FINAL_SUM, 2)
+
+print(f'\nYour total balance after one year with an initial investment compounded monthly of', MONTHLY_COMPOUNDED_BALANCES[1]-monthlyInterest, f'and APY of {APY} is ${rounded_sum}')
